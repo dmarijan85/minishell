1681d09a681bc09079a9b13ff79d8c976de6f6e8
mclaver-a <maxclau2002@gmail.com>
2024-11-06 18:25:04 +0100

    kk

--- cedepuntoce.c
+++ cedepuntoce.c
@@ -6,19 +6,27 @@
 /*   By: mclaver- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 13:58:41 by mclaver-          #+#    #+#             */
-/*   Updated: 2024/11/06 17:15:52 by mclaver-         ###   ########.fr       */
+/*   Updated: 2024/11/06 18:24:26 by mclaver-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-:#include "minishell.h"
+#include "minishell.h"
 
 //rutas absolutas Y relativas... creo que chdir ya controla bastante
 
-void	change_path(t_msh *mini)
+void	add_path_to_env(t_msh *mini)
 {
+	int	i;
 	char	*tmp;
 
-	tmp = ft_strdup(mini->pwd);
+	getenv
+}
+
+void	change_path(t_msh *mini)
+{
+//	mini-> OLDPWD tambien, el env no es necesariamente modificable no?
+	free(mini->pwd);
+	mini->pwd = getcwd(NULL, sizeof(NULL));
 }
 
 void	ft_cd(t_msh *mini, char **arr, int argc)
@@ -33,13 +41,13 @@ void	ft_cd(t_msh *mini, char **arr, int argc)
 //	change path a "OLDPWD=" same q con home
 //	else
 //	{
-		errnum = chdir(arr[1]);
-		if (errnum)
-			errexit(mini, "cagaste we (cd error)");
+	errnum = chdir(arr[1]);
+	if (errnum)
+		errexit(mini, "cagaste we (cd error)");
 			//error papito
 //	}
 	//vigilar el errnum de los 2 primeros ifs
-//change path(mini);
-//add path to env
+	change_path(mini);
+	add_path_to_env(mini);
 //return o lo q sea (TIENE QUE HACERLO EL PAPA)
 }
